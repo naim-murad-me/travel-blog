11655ed7f21c4bffbcc705a089f675382c891e7d
Naim Murad <Naim Murad@DESKTOP-S1I68CH>
2020-10-14 16:58:50 +0600

    * Added main web app

---
+++ travel-blog-web/src/main/java/org/travel/blog/web/controller/UserController.java
@@ -0,0 +1,74 @@
+package org.travel.blog.web.controller;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.travel.blog.api.service.SecurityService;
+import org.travel.blog.api.service.UserService;
+
+import com.nm.tb.models.User;
+
+@Controller
+@Aspect
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private SecurityService securityService;
+
+	@Autowired
+	private UserValidator userValidator;
+
+	@GetMapping("/registration")
+	public String registration(Model model) {
+		if (securityService.isAuthenticated()) {
+			return "redirect:/";
+		}
+
+		model.addAttribute("userForm", new User());
+
+		return "registration";
+	}
+
+	@PostMapping("/registration")
+	public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+		userValidator.validate(userForm, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
+
+		userService.save(userForm);
+
+		securityService.autoLogin(userForm.getUserName(), userForm.getPassword());
+
+		return "redirect:/welcome";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model, String error, String logout) {
+		if (securityService.isAuthenticated()) {
+			return "redirect:/";
+		}
+
+		if (error != null)
+			model.addAttribute("error", "Your username and password is invalid.");
+
+		if (logout != null)
+			model.addAttribute("message", "You have been logged out successfully.");
+
+		return "login";
+	}
+
+	@GetMapping({ "/", "/welcome" })
+	public String welcome(Model model) {
+		return "welcome";
+	}
+}
---
+++ travel-blog-web/src/test/java/org/travel/blog/web/AppTest.java
@@ -0,0 +1,14 @@
+package org.travel.blog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
